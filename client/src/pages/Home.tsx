47d34b804b2fd802cfac6958ce640ad0962cc465
비엔에스뮤직 <bns@inusmusic.kr>
2026-07-20 01:20:13 +0000

    refactor: VOTE ON VOICE 섹션을 사회자 선택(McSection) 앞으로 이동
    - 사회자 이름/사진 보고 고른 뒤에 블라인드 투표하면 의미 없으므로, 이름 노출 전에 투표 유도

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -42,8 +42,8 @@ export default function Home() {
       <PackageSection />
       <ProcessSection />
       <ReviewSection />
-      <McSection />
       <VoteOnVoiceSection />
+      <McSection />
       <VideoGuideSection />
       <BookingSection />
       <PricingSection />
